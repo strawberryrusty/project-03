7014e25554e218f6e14c29e98018411222c34fe5
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-08-15 17:05:43 +0100

    complete auth-controller

---
+++ controllers/auth.js
@@ -0,0 +1,29 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const { secret } = require('../config/environment')
+
+function registerRoute(req, res, next) {
+  User.create(req.body)
+    .then(() => res.json({ message: 'Registration successful' }))
+    .catch(next)
+}
+
+function loginRoute(req, res, next) {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      // if the user does not exist OR the user password is not valid
+      if(!user || !user.validatePassword(req.body.password)) {
+        return res.sendStatus(401) // send a 401 response
+      }
+      // generate a token
+      const token = jwt.sign({ sub: user._id }, secret, { expiresIn: '6h' })
+      // send it to the client
+      res.json({ message: `Welcome back ${user.username}!`, token })
+    })
+    .catch(next)
+}
+
+module.exports = {
+  register: registerRoute,
+  login: loginRoute
+}
--- models/User.js
+++ models/User.js
@@ -1,11 +1,65 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true }
+},{
+  toJSON: {
+    transform(doc, json) {
+      // ALWAYS remove the password when converting a user to JSON
+      delete json.password
+      delete json.__v
+      return json
+    }
+  }
 })
+
+// userSchema.virtual('fullname')
+//   .get(function getFullName() {
+//     return `${this.firstname} ${this.lastname}`
+//   })
+//   .set(function setFullName(fullname) {
+//     const [ firstname, lastname ] = fullname.split(' ')
+//     this.firstname = firstname
+//     this.lastname = lastname
+//   })
+
+// A virtual is data we need but don't want to store in the database
+userSchema.virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(plaintext) {
+    // plaintext is the value of passwordConfirmation
+    this._passwordConfirmation = plaintext
+  })
+
+// Pre Validate Middleware: logic that happens before the VALIDATE stage
+userSchema.pre('validate', function checkPasswords(next) {
+  // if the password and passwordConfirmation do not match
+  if(this.isModified('password') && this._passwordConfirmation !== this.password) {
+    // invalidate the data
+    this.invalidate('passwordConfirmation', 'Passwords do not match')
+  }
+
+  next() // Don't forget to call next!
+})
+
+userSchema.pre('save', function hashPassword(next) {
+  // if the password has been modified
+  if(this.isModified('password')) {
+    // hash it using 8 rounds of salt: BCRYPT
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8))
+  }
+
+  next()
+})
+
+// methods is like prototype for MONGOOSE
+// all users can now validate a password themselves
+userSchema.methods.validatePassword = function validatePassword(plaintext) {
+  return bcrypt.compareSync(plaintext, this.password)
+}
 
 
 
