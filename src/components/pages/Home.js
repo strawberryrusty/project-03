d983c31cdc67d2279096e3975398266351ae608e
Sian Alcock <sian.alcock@lawena.org>
2019-08-20 16:09:02 +0100

    Various styling changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,11 +4,9 @@ const Home = () => {
   return (
     <section className="hero is-large">
       <div className="hero-body">
-        <div className="container">
-          <h1 className="title is-1">
-            Lost the Plot
-          </h1>
-          <h2 className="subtitle is-4">Plants and stuff</h2>
+        <div className="title-container">
+          <h1 className="title is-1">Lost the plot</h1>
+          <h2 className="subtitle is-4">What to grow and where to grow it</h2>
         </div>
       </div>
     </section>
