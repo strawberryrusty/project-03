e0c019e2b4efbcb988ba422c43c85b1fc9d85dbb
Ejike Chiboka <ejike.chiboka@gmail.com>
2019-08-22 10:00:04 +0100

    footer styling

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -7,12 +7,22 @@ class Footer extends React.Component {
         <div className="footer-bottom">
           <div className="row">
             <div className="span">
+              <div id="footer-social">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.losttheplot.com" className="fab fa-3x fa-twitter-square"></a>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.losttheplot.com" className="fab fa-3x fa-facebook-square"></a>
+              </div>
               <p className="copyright"> © 2019. All rights reserved </p>
               <ul id="menu-footer-menu">
                 <li id="menu-item"> Privacy Policy </li>
-                <li id="menu-item"> Cookie Policy </li>
+                <li id="menu-item">Cookie Policy </li>
                 <li id="menu-item"> Terms and Conditions </li>
-                <p className="copyright"> This website uses cookies. By using this website, you agree we can set and use cookies </p>
+                <li id="menu-item"> Contact us </li>
+                <li id="menu-item"> Careers </li>
+                <li id="menu-item"> Advertise </li>
+                <li id="menu-item"> Events </li>
+                <div>
+                  <p className="copyright"> This website uses cookies. By using this website, you agree we can set and use cookies </p>
+                </div>
               </ul>
             </div>
           </div>
