ad1681c5c4505ada20f7fbc3269678b6176780e8
Prash Mohan <prashmohanuon@gmail.com>
2019-08-27 10:13:02 +0100

    corrected carrot png path

--- src/components/plots/Show.js
+++ src/components/plots/Show.js
@@ -189,7 +189,7 @@ class PlotsShow extends React.Component {
                 <Marker
                   coordinates={[this.state.plot.longitude, this.state.plot.latitude]}
                 >
-                  <img src={'../../assets/Carrot.png'}
+                  <img src={'../assets/Carrot.png'}
                     height='50'
                     width='50'
                   />
